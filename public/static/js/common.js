9c8337b6ed4af6788b0765dfd7f9258c4e632320
zhouyuchao <392318709@qq.com>
2017-10-18 09:02:49 +0800

    add public

---
+++ public/static/js/common.js
@@ -0,0 +1,55 @@
+$(function () {
+    $('#logout').click(function () {
+        layer.confirm('确定退出登录吗？',
+                {
+                    btn: ['确定', '取消'] //按钮
+                },
+                function () {
+                    $.post("/doLogout", function (data) {
+                        if (data.code === 200) {
+                            window.location.href = "/index.html";
+                        }
+                    }
+                    );
+                });
+    });
+
+    //顶部导航切换
+    $(".nav li a").click(function () {
+        $(".nav li a.selected").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    //导航切换
+    $(".menuson .header").click(function () {
+        var $parent = $(this).parent();
+        $(".menuson>li.active").not($parent).removeClass("active open").find('.sub-menus').hide();
+
+        $parent.addClass("active");
+        if (!!$(this).next('.sub-menus').size()) {
+            if ($parent.hasClass("open")) {
+                $parent.removeClass("open").find('.sub-menus').hide();
+            } else {
+                $parent.addClass("open").find('.sub-menus').show();
+            }
+
+
+        }
+    });
+
+    // 三级菜单点击
+    $('.sub-menus li').click(function (e) {
+        $(".sub-menus li.active").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $('.title').click(function () {
+        var $ul = $(this).next('ul');
+        $('dd').find('.menuson').slideUp();
+        if ($ul.is(':visible')) {
+            $(this).next('.menuson').slideUp();
+        } else {
+            $(this).next('.menuson').slideDown();
+        }
+    });
+});
